70b1ff725eec2263679c937edfc22a1e65f92ad0
Charalampos Fanoulis <yo@fanoulis.dev>
2020-12-06 22:34:40 +0200

    chore: add challenge template

---
+++ src/template/index.ts
@@ -0,0 +1,16 @@
+import { loadInput } from './util/loadInput.js';
+
+// const require = createRequire(import.meta.url);
+
+const i = await loadInput();
+
+function firstPart() {
+	return `[Part 1] Not implemented yet!`;
+}
+
+function secondPart() {
+	return `[Part 2] Not implemented yet!`;
+}
+
+console.log(firstPart());
+console.log(secondPart());
